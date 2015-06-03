630a3c15defccecb62f453bfd26680a0945240e2
SaintEmbers <quantum108@gmail.com>
2015-06-03 13:06:16 -0600

    initial setup

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  ClientID: "Kg6eb8DhdcqRvOn6inTFMaVBo5TWU1YS",
+  ClientSecret: "KdSfX30PGw1_XV1vchKAI8i289tYS_I8lOyTUxc1",
+  ServerID: "ZKmMtzv-exB3SOLPNlBIl7TY-z6fzedMDr90bvqF"
+}
